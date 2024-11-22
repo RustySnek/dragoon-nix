6958703d7b4d8d7ab1cc131f1b2ccfc4c09e362b
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-11-22 19:14:06 +0100

    add 24.05 nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
   nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  nixpkgs-legacy.url = "github:nixos/nixpkgs/nixos-24.05";
   nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   nil-ls.url = "github:oxalica/nix";
   home-manager.url = "github:nix-community/home-manager/release-24.11";
@@ -9,10 +10,10 @@
   nur.url = "github:nix-community/NUR";
   disko.url = "github:nix-community/disko";
   disko.inputs.nixpkgs.follows = "nixpkgs";
-  # tf to do with disko?
 };
   outputs = inputs @ {
     nixpkgs,
+    nixpkgs-legacy,
     nixpkgs-unstable,
     nil-ls,
     disko,
@@ -24,7 +25,7 @@
   nixosConfigurations = (
     import ./hosts {
       inherit (nixpkgs) lib;
-      inherit inputs disko nixpkgs nixpkgs-unstable home-manager impermanence user nur nil-ls;
+      inherit inputs disko nixpkgs nixpkgs-legacy nixpkgs-unstable home-manager impermanence user nur nil-ls;
     }
   );
 };
